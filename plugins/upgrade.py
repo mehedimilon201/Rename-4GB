9a0d8d7e4c6b022d63bd4173f2c685c191eb54ec
mehedimilon20 <mehedimilon20@gmail.com>
2025-06-24 02:15:52 +0600

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -23,14 +23,14 @@ Daily Upload limit 100GB
 Price Rs 179  ind /🌎 2.16$  per Month
 
 Payment Details :-
-<b>➜ UPI ID :</b> <code>madflixofficial@axl</code>
-<b>➜ PayPal :</b> <a href='https://www.paypal.me/jishudeveloper'>Click Here</a>
+<b>➜ Bkash Merchant Number:</b> <code>01316597665</code>
+<b>➜ PayPal :</b> <a href='https://www.paypal.me/'>Click Here</a>
 <b>➜ QR Code :</b> <a href='https://telegra.ph/QR-Payment-07-24-4'>Click Here</a>
 
 After Payment Send Screenshots Of Payment To Admin @MadflixOfficials"""
     
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/calladminrobot"),
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/milonflix_bot"),
         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
         ])
     
