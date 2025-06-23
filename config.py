9a1e248ea656fd07ab3611ed56a1e936fd369b3c
mehedimilon20 <mehedimilon20@gmail.com>
2025-06-24 02:03:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "23830854"))
+API_HASH = os.environ.get("API_HASH", "2360d53e98bf778a286d8c054333d470")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7566326429:AAFUEltXGqiNTp4pFHFa8bRtW7HAjI51ntA")
+ADMIN = int(os.environ.get("ADMIN", "1791428788"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQFroUYARf0GwIiMyeWnNX7nFrOlxfqIvVmRGpfx3Id1tnUBMPLCQwwvYc7_keThp6yHm7igesqkY2CC7pcsbz4_jAIW1KC9g-bLzSY9INqofxLLmOeNo0Qc2bTk51xpAM4E-4SEcJ9veyFcWYkGcTq52-hGAkeHQLwXl2pf0_JHQoyqEN5QlH_K-nFTxX4PDS74_MybdLACr7trwCGSUsNDBVHdvch-0NFCdQIOJsdHWGz1L9l3j-PQgKHOewSrxWP0WM8Zyly2HxOlYXgICV19kW-ALW-oXh0Zd1njdgaZR0RRFxQR663YaI4ms3skQnfGgv5zIJr_A5Uo3jvZM8abvRttFQAAAAGwmuuNAA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002682807987")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002592791382"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mehedimilon201:mehedimilon201@cluster0.czbmlbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 
@@ -38,9 +38,9 @@ START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d
 # Token Verification Adding Features Is Paid So If You Want Then Dm Me
 
 
-# SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-# SHORTNER_API = os.environ.get("SHORTNER_API", "")
-# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+# SHORTNER_URL = os.environ.get("SHORTNER_URL", "linkshortify.com")
+# SHORTNER_API = os.environ.get("SHORTNER_API", "4cf15e1106729ec5c145c28da45ceea4cba0f7c5")
+# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "300")
 
 
 
